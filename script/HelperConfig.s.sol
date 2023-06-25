9d80bbf806893f66e822dbfc0d4c8ff3ecd480e4
Yash Garg <yashygkf@gmail.com>
2023-06-25 15:38:04 +0530

    update: Helper config with latest OZ version

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -21,35 +21,37 @@ contract HelperConfig is Script {
 
     NetworkConfig public activeNetworkConfig;
 
-    constructor (){
-        if(block.chainid == 11155111) {
+    constructor() {
+        if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
             activeNetworkConfig = getOrCreateAnvilEthConfig();
-            }
+        }
     }
 
-    function getSepoliaEthConfig () public view returns (NetworkConfig memory) {
-        return NetworkConfig ({
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return NetworkConfig({
             wethPriceFeedAddress: 0x694AA1769357215DE4FAC081bf1f309aDC325306,
             wbtcPriceFeedAddress: 0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43,
             wethAddress: 0xdd13E55209Fd76AfE204dBda4007C227904f0a81,
             wbtcAddress: 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063,
             deployerKey: vm.envUint("PRIVATE_KEY")
-        })
+        });
     }
 
-    function getOrCreateAnvilEthConfig() public returns( NetworkConfig memory) {
-        if( activeNetworkConfig.wethPriceFeedAddress != address(0)) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.wethPriceFeedAddress != address(0)) {
             return activeNetworkConfig;
         }
 
         vm.startBroadcast();
         MockV3Aggregator ethPriceFeed = new MockV3Aggregator(DECIMALS, ETH_PRICE);
-        ERC20Mock weth = new ERC20Mock("Wrapped Ether", "WETH", msg.sender, 1000e8);
+        ERC20Mock weth = new ERC20Mock();
+        weth.mint(msg.sender, 1000e8);
 
         MockV3Aggregator btcPriceFeed = new MockV3Aggregator(DECIMALS, ETH_PRICE);
-        ERC20Mock wbtc = new ERC20Mock("Wrapped BTC", "WBTC", msg.sender, 1000e8);
+        ERC20Mock wbtc = new ERC20Mock();
+        wbtc.mint(msg.sender, 1000e8);
 
         vm.stopBroadcast();
 
@@ -59,6 +61,6 @@ contract HelperConfig is Script {
             wethAddress: address(weth),
             wbtcAddress: address(wbtc),
             deployerKey: PRIVATE_KEY
-        })
+        });
     }
 }
