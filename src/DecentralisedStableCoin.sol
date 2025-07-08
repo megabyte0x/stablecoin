85466382bf9b4f54b55dee5ebe57c6b36077efd5
megabyte0x <79016290+megabyte0x@users.noreply.github.com>
2025-07-08 17:35:20 +0530

    init

---
+++ src/DecentralisedStableCoin.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.30;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title DecentralisedStableCoin
+ *     @author @megabyte0x
+ */
+contract DecentralisedStableCoin {}
